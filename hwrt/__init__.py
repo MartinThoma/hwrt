86d8fbc8f3ed31da503e12bdb2c39ff087492748
Martin Thoma <info@martin-thoma.de>
2014-10-07 16:09:23 +0200

    __init__.py: Error handling for version attribute

--- hwrt/__init__.py
+++ hwrt/__init__.py
@@ -1,3 +1,15 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
 
-__version__ = get_distribution('hwrt').version
+try:
+    _dist = get_distribution('hwrt')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'hwrt')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'hwrt',
-    'version': '0.1.14',
+    'version': '0.1.15',
     'author': 'Martin Thoma',
     'author_email': 'info@martin-thoma.de',
     'packages': ['hwrt'],

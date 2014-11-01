afb74d91df87c1cbcfc8c8a82b2e695026a197c8
Martin Thoma <info@martin-thoma.de>
2014-11-02 00:03:22 +0100

    added selfcheck and create_model test

---
+++ tests/create_model_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nose
+import os
+
+# hwrt modules
+import hwrt.create_model as create_model
+import hwrt.utils as utils
+
+
+# Tests
+def execution_test():
+    small = os.path.join(utils.get_project_root(), "models/small-baseline")
+    create_model.main(small)
---
+++ tests/selfcheck_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nose
+
+# hwrt modules
+import hwrt.selfcheck as selfcheck
+
+
+# Tests
+def execution_test():
+    selfcheck.main()

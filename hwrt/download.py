1ce03ceb01415753e2afac5fb7637b92b443b397
Isko <iskren.jgd@gmail.com>
2015-04-08 21:52:15 +0300

    hwrt/download.py: avoid big file MemoryError
    https://github.com/MartinThoma/hwrt/issues/17#issuecomment-90996381

--- hwrt/download.py
+++ hwrt/download.py
@@ -14,11 +14,22 @@ import urllib
 # hwrt modules
 import hwrt.utils as utils
 
+def generate_file_md5(local_path_file, blocksize=2**20):
+    # http://stackoverflow.com/a/1131255/3345926
+    m = hashlib.md5()
+    with open( local_path_file , "rb" ) as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            m.update( buf )
+    return m.hexdigest()
 
 def is_file_consistent(local_path_file, md5_hash):
     """Check if file is there and if the md5_hash is correct."""
     return os.path.isfile(local_path_file) and \
-        hashlib.md5(open(local_path_file, 'rb').read()).hexdigest() == md5_hash
+        generate_file_md5(local_path_file) == md5_hash
+        # hashlib.md5(open(local_path_file, 'rb').read()).hexdigest() == md5_hash
 
 
 def get_parser():

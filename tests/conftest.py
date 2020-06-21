eae4b4393f7aed4e02f6b19e40631a1d4ae31bac
Martin Thoma <info@martin-thoma.de>
2020-06-21 13:43:11 +0200

    TST: Silence Flake8

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+# Core Library modules
+import logging
+
+
+def pytest_configure(config):
+    """Flake8 is very verbose by default. Silence it."""
+    logging.getLogger("flake8").setLevel(logging.WARNING)
